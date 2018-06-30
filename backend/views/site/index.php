b3380b8aa5b597087f18dcb23f78928cad617882
BorisMatic <boki.no.1@hotmail.com>
2018-06-30 19:47:50 +0200

    Changed periods.

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -154,7 +154,7 @@ $this->title = 'Freak pay';
                         'data' => [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4],
                         'tooltip' => [
                             //      'valueSuffix' => ' mm'
-                        ]
+                        ],
 
                     ], [
                         'name' => 'Earnings',
@@ -176,22 +176,32 @@ $this->title = 'Freak pay';
 
 $js = <<<JSHIGHCHARTS
 console.log('radi');
+var charts = [
+{
+    id : "#w0",
+    period : "hours_1"
+},
+{
+    id : "#w1",
+    period : "hours_6"
+},
+{
+    id : "#w2",
+    period : "hours_24"
+}
+];
 function requestData() {
     $.ajax({
         url: '/statistics/graph-info',
         success: function(data) {
-            var chart = $("#w0").highcharts();
-            chart.series[0].setData(data['hours_4'][0]);
-            chart.series[1].setData(data['hours_4'][1]);
-            
-            var chart = $("#w1").highcharts();
-            chart.series[0].setData(data['hours_8'][0]);
-            chart.series[1].setData(data['hours_8'][1]);
-            
-            var chart = $("#w2").highcharts();
-            chart.series[0].setData(data['hours_24'][0]);
-            chart.series[1].setData(data['hours_24'][1]);
-            
+        
+        for(i in charts) {
+        console.log(charts[i])
+            var id = charts[i].id;
+            var chart = $(id).highcharts();
+            chart.series[0].setData(data[charts[i].period][0]);
+            chart.series[1].setData(data[charts[i].period][1]);
+        }   
             // call it again after one second
             setTimeout(requestData, 1000);    
         },
