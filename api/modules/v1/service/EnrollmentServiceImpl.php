c96af163aea16808ba6275111c662451668ef024
jasmin.suljic <jasmin.suljich@gmail.com>
2018-06-30 16:44:56 +0200

    Enroll

---
+++ api/modules/v1/service/EnrollmentServiceImpl.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jasminsuljic
+ * Date: 30/06/2018
+ * Time: 16:38
+ */
+
+class EnrollmentServiceImpl implements EnrollmentService
+{
+
+    /**
+     *
+     * Return ['status' => 'enrolled | not_enrolled', '?user_id']
+     *
+     * @param $id
+     * @param $type
+     * @return mixed
+     */
+    public function enrollmentStatus($id, $type)
+    {
+        // TODO: Implement enrollmentStatus() method.
+    }
+}
---
+++ api/modules/v1/service/OrderServiceImpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: jasminsuljic
+ * Date: 30/06/2018
+ * Time: 16:38
+ */
+
+class OrderServiceImpl implements OrderService
+{
+
+    public function payWithCreditCard($orderNumber, NewCreditCardPayment $newCreditCardPayment)
+    {
+        // TODO: Implement payWithCreditCard() method.
+    }
+
+    public function payWithSavedCreditCard($orderNumber, SavedCreditCardPayment $savedCreditCardPayment)
+    {
+        // TODO: Implement payWithSavedCreditCard() method.
+    }
+
+    public function payWithEwallet($orderNumber, EWalletPayment $payment)
+    {
+        // TODO: Implement payWithEwallet() method.
+    }
+
+    public function getPendingOrder($orderNumber)
+    {
+        // TODO: Implement getPendingOrder() method.
+    }
+
+    public function getCompletedOrder($orderNumber)
+    {
+        // TODO: Implement getCompletedOrder() method.
+    }
+}
