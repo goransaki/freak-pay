119ddb807b6e4808b1292e4c4baeda430713ac0d
jasmin.suljic <jasmin.suljich@gmail.com>
2018-06-30 20:32:46 +0200

    User controller implementation

--- console/migrations/schema.sql
+++ console/migrations/schema.sql
@@ -1,165 +1,199 @@
-create table payment_method
-(
-  id           int auto_increment
-    primary key,
-  identifier   varchar(500)            not null,
-  type         varchar(30)             null,
-  created_at   datetime                not null,
-  updated_at   datetime                not null,
-  user_id      int                     not null,
-  sort_key     smallint(6) default '0' not null,
-  auth_enabled tinyint(1)              null
-);
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.55-0ubuntu0.14.04.1 - (Ubuntu)
+-- Server OS:                    debian-linux-gnu
+-- HeidiSQL Verzija:             9.5.0.5196
+-- --------------------------------------------------------
 
-create table product
-(
-  id    int auto_increment
-    primary key,
-  code  varchar(255) not null,
-  name  varchar(255) not null,
-  price int          not null
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
-create table secure_data
-(
-  token                 varchar(36)  not null
-    primary key,
-  data                  text         null,
-  secure_vault_provider varchar(500) null,
-  created_at            datetime     not null,
-  updated_at            datetime     not null,
-  exp_date              datetime     null
-);
+-- Dumping structure for table freak-pay.bank_account
+CREATE TABLE IF NOT EXISTS `bank_account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `token_id` varchar(36) NOT NULL,
+  `name` varchar(30) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `bank_account_user_id_fk` (`user_id`),
+  KEY `bank_account_secure_data_token_fk` (`token_id`),
+  CONSTRAINT `bank_account_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `bank_account_secure_data_token_fk` FOREIGN KEY (`token_id`) REFERENCES `secure_data` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table store
-(
-  id         int auto_increment
-    primary key,
-  identifier varchar(255)  not null,
-  name       varchar(255)  not null,
-  latitude   varchar(20)   not null,
-  longitude  varchar(20)   not null,
-  address    varchar(2000) not null,
-  created_at datetime      not null
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.card
+CREATE TABLE IF NOT EXISTS `card` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `token_id` varchar(36) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `card_user_id_fk` (`user_id`),
+  KEY `card_secure_data_token_fk` (`token_id`),
+  CONSTRAINT `card_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `card_secure_data_token_fk` FOREIGN KEY (`token_id`) REFERENCES `secure_data` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table orders
-(
-  id         int auto_increment
-    primary key,
-  identifier varchar(255) not null,
-  store_id   int          not null,
-  created_at datetime     not null,
-  updated_at datetime     not null,
-  status     varchar(200) null,
-  constraint order_store_id_fk
-  foreign key (store_id) references store (id)
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.crypto_currency
+CREATE TABLE IF NOT EXISTS `crypto_currency` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `token_id` varchar(36) NOT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `crypto_currency_user_id_fk` (`user_id`),
+  KEY `crypto_currency_secure_data_token_fk` (`token_id`),
+  CONSTRAINT `crypto_currency_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `crypto_currency_secure_data_token_fk` FOREIGN KEY (`token_id`) REFERENCES `secure_data` (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table order_product
-(
-  id         int auto_increment
-    primary key,
-  order_id   int         null,
-  product_id int         null,
-  quantity   smallint(6) null,
-  constraint order_product_order_id_fk
-  foreign key (order_id) references orders (id),
-  constraint order_product_product_id_fk
-  foreign key (product_id) references product (id)
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.device
+CREATE TABLE IF NOT EXISTS `device` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nfc_data` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `device_user_id_fk` (`user_id`),
+  CONSTRAINT `device_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table user
-(
-  id                   int auto_increment
-    primary key,
-  username             varchar(255)             not null,
-  auth_key             varchar(32)              not null,
-  password_hash        varchar(255)             not null,
-  password_reset_token varchar(255)             null,
-  email                varchar(255)             not null,
-  status               smallint(6) default '10' not null,
-  created_at           int                      not null,
-  updated_at           int                      not null,
-  constraint username
-  unique (username),
-  constraint password_reset_token
-  unique (password_reset_token),
-  constraint email
-  unique (email)
-)
-  collate = utf8_unicode_ci;
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.migration
+CREATE TABLE IF NOT EXISTS `migration` (
+  `version` varchar(180) NOT NULL,
+  `apply_time` int(11) DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table bank_account
-(
-  id         int auto_increment
-    primary key,
-  user_id    int         not null,
-  token_id   varchar(36) not null,
-  name       varchar(30) null,
-  created_at datetime    not null,
-  updated_at datetime    not null,
-  constraint bank_account_user_id_fk
-  foreign key (user_id) references user (id),
-  constraint bank_account_secure_data_token_fk
-  foreign key (token_id) references secure_data (token)
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.orders
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) NOT NULL,
+  `store_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `status` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_store_id_fk` (`store_id`),
+  CONSTRAINT `order_store_id_fk` FOREIGN KEY (`store_id`) REFERENCES `store` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
-create table card
-(
-  id         int auto_increment
-    primary key,
-  user_id    int         not null,
-  token_id   varchar(36) not null,
-  created_at datetime    not null,
-  updated_at datetime    not null,
-  constraint card_user_id_fk
-  foreign key (user_id) references user (id),
-  constraint card_secure_data_token_fk
-  foreign key (token_id) references secure_data (token)
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.order_product
+CREATE TABLE IF NOT EXISTS `order_product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) DEFAULT NULL,
+  `product_id` int(11) DEFAULT NULL,
+  `quantity` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `order_product_order_id_fk` (`order_id`),
+  KEY `order_product_product_id_fk` (`product_id`),
+  CONSTRAINT `order_product_order_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `order_product_product_id_fk` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
 
-create table crypto_currency
-(
-  id         int auto_increment
-    primary key,
-  user_id    int         not null,
-  token_id   varchar(36) not null,
-  type       varchar(30) null,
-  created_at datetime    not null,
-  updated_at datetime    not null,
-  constraint crypto_currency_user_id_fk
-  foreign key (user_id) references user (id),
-  constraint crypto_currency_secure_data_token_fk
-  foreign key (token_id) references secure_data (token)
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.payment_method
+CREATE TABLE IF NOT EXISTS `payment_method` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(500) NOT NULL,
+  `type` varchar(30) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `sort_key` smallint(6) NOT NULL DEFAULT '0',
+  `auth_enabled` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-create table device
-(
-  id         int auto_increment
-    primary key,
-  nfc_data   text     null,
-  created_at datetime not null,
-  updated_at datetime not null,
-  user_id    int      not null,
-  constraint device_user_id_fk
-  foreign key (user_id) references user (id)
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.product
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
 
-create table transaction
-(
-  id                int auto_increment
-    primary key,
-  order_id          int      not null,
-  user_id           int      null,
-  created_at        datetime not null,
-  updated_at        datetime not null,
-  payment_method_id int      not null,
-  constraint transaction_order_id_fk
-  foreign key (order_id) references orders (id),
-  constraint transaction_user_id_fk
-  foreign key (user_id) references user (id),
-  constraint transaction_payment_method_id_fk
-  foreign key (payment_method_id) references payment_method (id)
-);
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.secure_data
+CREATE TABLE IF NOT EXISTS `secure_data` (
+  `token` varchar(36) NOT NULL,
+  `data` text,
+  `secure_vault_provider` varchar(500) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `exp_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`token`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.store
+CREATE TABLE IF NOT EXISTS `store` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `latitude` varchar(20) NOT NULL,
+  `longitude` varchar(20) NOT NULL,
+  `address` varchar(2000) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.transaction
+CREATE TABLE IF NOT EXISTS `transaction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `payment_method_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `transaction_order_id_fk` (`order_id`),
+  KEY `transaction_user_id_fk` (`user_id`),
+  KEY `transaction_payment_method_id_fk` (`payment_method_id`),
+  CONSTRAINT `transaction_order_id_fk` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  CONSTRAINT `transaction_user_id_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `transaction_payment_method_id_fk` FOREIGN KEY (`payment_method_id`) REFERENCES `payment_method` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Data exporting was unselected.
+-- Dumping structure for table freak-pay.user
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `auth_key` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `password_hash` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password_reset_token` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `status` smallint(6) NOT NULL DEFAULT '10',
+  `created_at` int(11) NOT NULL,
+  `updated_at` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `password_reset_token` (`password_reset_token`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
