ca3340e468a2f309316fc98b1d8a3f353e8d6e5f
Goran <goransarenac91gmail.com>
2018-07-01 10:43:23 +0200

    Added transactions on dashboard.

--- backend/views/site/partials/transaction_list.php
+++ backend/views/site/partials/transaction_list.php
@@ -1,6 +1,6 @@
 <div class="col-md-6">
     <div class="m-portlet m-portlet--tabs m-portlet--success m-portlet--head-solid-bg m-portlet--bordered">
-        <?= $this->render('transaction_detail') ?>
+        <?= $this->render('transaction_header', ['model' => $model]) ?>
         <?= $this->render('order_detail', ['model' => $model]) ?>
     </div>
 </div>
